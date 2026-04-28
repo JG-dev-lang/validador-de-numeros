3b753c8fbfaf9cb5bd4ef382f062393f15f00b90
JG-dev-lang <58432190+JG-dev-lang@users.noreply.github.com>
2026-04-28 14:49:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
+import os
 
 app = Flask(__name__)
 
-# Banco de dados fake
 produtos = [
     {"id": 1, "nome": "Camiseta", "preco": 49.90},
-    {"id": 2, "nome": "Tênis", "preco": 199.90},
-    {"id": 3, "nome": "Boné", "preco": 39.90}
 ]
 
 @app.route("/")
 def home():
-    return "<h1>Loja Genérica do JOTAGE 🛒🔥</h1>"
+    return render_template("index.html")
 
-@app.route("/produtos")
+@app.route("/produtos", methods=["GET"])
 def listar_produtos():
     return jsonify(produtos)
 
+@app.route("/produtos", methods=["POST"])
+def adicionar_produto():
+    novo = request.json
+    novo["id"] = len(produtos) + 1
+    produtos.append(novo)
+    return jsonify(novo)
+
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
