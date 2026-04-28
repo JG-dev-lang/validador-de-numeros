f183df6cd069ee0141b91dc99a6e2ddcff469e83
JG-dev-lang <58432190+JG-dev-lang@users.noreply.github.com>
2026-04-28 14:39:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
-import os
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# Banco de dados fake
+produtos = [
+    {"id": 1, "nome": "Camiseta", "preco": 49.90},
+    {"id": 2, "nome": "Tênis", "preco": 199.90},
+    {"id": 3, "nome": "Boné", "preco": 39.90}
+]
+
 @app.route("/")
 def home():
-    return "Pipeline + CD funcionando 😈"
+    return "<h1>LojaGenérica do JOTAGE 🛒🔥</h1>"
+
+@app.route("/produtos")
+def listar_produtos():
+    return jsonify(produtos)
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
